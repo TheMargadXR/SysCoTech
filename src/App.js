218afb361caf0a7f6bb4fbb1538aa5b2061fa96c
The Margad <122777402+TheMargadXR@users.noreply.github.com>
2024-03-11 00:13:48 +0800

    ch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React from 'react';
-import Navbar from './Components/Navbar';
-import Hero from './Components/Hero';
-import Card from './Components/Card';
-import Extra from './Components/Extra';
-import Products from './Components/Products';
-import Footer from './Components/Footer';
-
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import Card from "./Components/Card";
+import Extra from "./Components/Extra";
+import Products from "./Components/Products";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <div className='block gap-40'>
-    <h1>checkout</h1>
+    <div className="block gap-40">
       <Navbar />
       <Hero />
       <Card />
       <Extra />
       <Products />
       <Footer />
-     
     </div>
   );
 }
